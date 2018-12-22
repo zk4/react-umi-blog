59ef0e20514c1227a8254eb70e9ebaabde2be97b
zk <liuzq7@gmail.com>
2018-12-22 12:49:53 +0800

    clean

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,18 +1,13 @@
 import React from 'react';
-// import ReactDOM   from'react-dom'
 import ReactMarkdown from 'react-markdown/with-html';
+import axios from 'axios';
 import * as showdown from 'showdown';
 import   showdownHighlight from "showdown-highlight"
+
 const converter = new showdown.Converter({
   extensions: [showdownHighlight]
 });
 
-// .Converter();
-
-
-import axios from 'axios';
-const input = '# This is a header\n\nAnd this is a paragraph';
-//https://raw.githubusercontent.com/rexxars/react-markdown/master/README.md
 export default class Welcome extends React.Component {
   constructor(props) {
     super(props);
@@ -24,9 +19,6 @@ export default class Welcome extends React.Component {
     axios
     .get('https://raw.githubusercontent.com/rexxars/react-markdown/master/README.md')
     .then(data => {
-      // converter = new showdown.Converter(),
-      // text      = '# hello, markdown!',
-      // html      = converter.makeHtml(text);
         this.setState({ md: converter.makeHtml(data.data) });
 
       })
@@ -37,16 +29,14 @@ export default class Welcome extends React.Component {
   }
   render() {
     return (
-      <p>
-        想要添加更多页面？请参考 
+      <div>
         {/* <ReactMarkdown source={this.state.md}  escapeHtml={false} /> */}
-        {/* {this.state.md} */}
         <div dangerouslySetInnerHTML={{ __html: this.state.md }} />
         <a href="https://umijs.org/guide/block.html" target="_blank" rel="noopener noreferrer">
           umi 区块
         </a>
         。
-      </p>
+      </div>
     );
   }
 }
